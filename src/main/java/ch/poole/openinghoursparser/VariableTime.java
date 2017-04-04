873b95425af8030baa22a080cbfdf83659224d9f
simonpoole <simon@poole.ch>
2017-04-04 15:21:13 +0200

    Fix issue with output of VariableTime with offset.

--- src/main/java/ch/poole/openinghoursparser/VariableTime.java
+++ src/main/java/ch/poole/openinghoursparser/VariableTime.java
@@ -29,14 +29,18 @@ public class VariableTime extends Element {
 	public String toString() {
 		StringBuilder b = new StringBuilder();
 		if (event != null) {
-			b.append(event);
 			if (offset != 0) {
+				b.append("(");
+				b.append(event);
 				if (offset > 0) {
 					b.append("+");
 				} 
 				b.append(String.format("%02d",(int)offset/60));
 				b.append(":");
 				b.append(String.format("%02d",Math.abs(offset)%60));
+				b.append(")");
+			} else {
+				b.append(event);
 			}
 		}
 		return b.toString();		
