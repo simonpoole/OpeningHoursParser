38c082654c98793d716b5d1021ed4e925c8bef8a
Vuong Ho <hochivuong2002@gmail.com>
2021-04-12 16:27:52 +0700

    Add strict option for individual testing

--- src/test/java/ch/poole/openinghoursparser/IndividualTest.java
+++ src/test/java/ch/poole/openinghoursparser/IndividualTest.java
@@ -1,4 +1,3 @@
-
 package ch.poole.openinghoursparser;
 
 import java.io.ByteArrayInputStream;
@@ -6,7 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 
 /**
- * Individual testing for the OpeningHoursParser receiving
+ * Individual testing for the OpeningHoursParser, receiving
  * inputs from System.in
  * 
  * 
@@ -20,24 +19,31 @@ public class IndividualTest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); 
         
+        Boolean isStrict = (args[0].equals("true")) ? true : false;
+
         while(true) {
             System.out.print("Please enter your input: ");
             String input = sc.nextLine();
 
             try {
                 OpeningHoursParser parser = new OpeningHoursParser(new ByteArrayInputStream(input.getBytes()));
-                List<ch.poole.openinghoursparser.Rule> rules = parser.rules(false);
+                List<ch.poole.openinghoursparser.Rule> rules = parser.rules(isStrict);
                 
                 System.out.println("Legal input string");
-                System.out.println("Detected rules in input string");
+                System.out.println("Detected rules in input string listed below");
                 for(ch.poole.openinghoursparser.Rule rule : rules) {
                     System.out.println(rule.toDebugString());
                 }
-    
+                System.out.println();
+                System.out.println("------------------------------");
+                System.out.println();
     
             } catch (OpeningHoursParseException e) {
                 System.out.println("Illegal input string");
                 e.printStackTrace();
+                System.out.println();
+                System.out.println("------------------------------");
+                System.out.println();
             }
         }
         
