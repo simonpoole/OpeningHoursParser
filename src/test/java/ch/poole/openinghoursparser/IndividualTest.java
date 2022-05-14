5a19ecb718a86e1a8414b6ac6d69fabf761f8090
simonpoole <simon@poole.ch>
2022-05-14 15:18:29 +0200

    Allow empty input to terminate testing

--- src/test/java/ch/poole/openinghoursparser/IndividualTest.java
+++ src/test/java/ch/poole/openinghoursparser/IndividualTest.java
@@ -5,27 +5,30 @@ import java.util.List;
 import java.util.Scanner;
 
 /**
- * Individual testing for the OpeningHoursParser, receiving
- * inputs from System.in
+ * Individual testing for the OpeningHoursParser, receiving inputs from System.in
  * 
  * 
  * @author Vuong Ho
  *
  */
-
 public class IndividualTest {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); 
+        Scanner sc = new Scanner(System.in);
         Boolean isStrict = args[0].equals("true");
-        while(true) {
+        System.out.println("Parse strings in opening-hours format, empty input will terminate");
+        while (true) {
             System.out.print("Please enter your input: ");
             String input = sc.nextLine();
+            if ("".equals(input)) {
+                break;
+            }
             try {
                 OpeningHoursParser parser = new OpeningHoursParser(new ByteArrayInputStream(input.getBytes()));
                 List<ch.poole.openinghoursparser.Rule> rules = parser.rules(isStrict);
                 System.out.println("Legal input string");
                 System.out.println("Detected rules in input string listed below");
-                for(ch.poole.openinghoursparser.Rule rule : rules) {
+                System.out.println("\n------------------------------\n");
+                for (ch.poole.openinghoursparser.Rule rule : rules) {
                     System.out.println(rule.toDebugString());
                 }
                 System.out.println("\n------------------------------\n");
